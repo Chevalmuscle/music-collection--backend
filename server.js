45468f2765f32653ba9ae004db396e7f55438774
chevalmuscle <marcantoinejean1997@gmail.com>
2019-12-27 10:32:32 -0500

    unblocked cors

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require("express");
+var cors = require('cors');
 const db = require("./db");
 
 const app = express();
 const PORT = process.env.PORT || 3001
+
+app.use(cors({origin: '*'}));
 
 app.get("/", (req, res) => {
   res.send("hello world !");
